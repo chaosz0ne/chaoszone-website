1377f83e52a95058d810be6a6ed16afd1ceb3e0f
nek0 <nek0@nek0.eu>
2019-12-19 22:33:17 +0100

    switch to haskell.nix build infrastructure

--- shell.nix
+++ shell.nix
@@ -1,33 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
-
 let
-
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, aeson, base, filepath, hakyll, HTTP, stdenv
-      , time
-      }:
-      mkDerivation {
-        pname = "chaoszone";
-        version = "0.0.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [
-          aeson base filepath hakyll HTTP time
-        ];
-        homepage = "https://chaoszone.cz";
-        license = stdenv.lib.licenses.publicDomain;
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
+  hsPkgs = import ./default.nix {};
 in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+  hsPkgs.chaoszone.components.all
