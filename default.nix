5e5cf69b8eae67cc64000d907913d889bcaa3995
Daniel Havlik <dh@gocept.com>
2026-02-18 19:22:26 +0100

    remove old stuff

--- default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {}
-, self ? ./., packageName ? "chaoszone"
-}:
-let
-  gitignore = dir: pkgs.nix-gitignore.gitignoreSource [] dir;
-in
-  pkgs.haskellPackages.callCabal2nix packageName (gitignore self) {}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {}
-, chaoszone ? import ./default.nix { inherit pkgs; packageName = "chaoszone"; }
-}:
-
-pkgs.mkShell {
-  packages = with pkgs; [
-    hugo
-  ];
-}
